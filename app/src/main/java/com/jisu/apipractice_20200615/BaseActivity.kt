acea3f35de1f22281208a101c57911fb17de246f
JISU LEE <jsnote2013@gmail.com>
2020-06-15 19:47:09 +0900

    메인 액티비티->로그인 액티비티로 변경

---
+++ app/src/main/java/com/jisu/apipractice_20200615/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.jisu.apipractice_20200615
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
--- app/src/main/java/com/jisu/apipractice_20200615/MainActivity.kt
+++ app/src/main/java/com/jisu/apipractice_20200615/MainActivity.kt
@@ -3,10 +3,20 @@ package com.jisu.apipractice_20200615
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }

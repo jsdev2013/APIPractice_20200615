71b73f7c14d55902c661e95feb62029e134a75e0
JISU LEE <jsnote2013@gmail.com>
2020-06-24 19:53:05 +0900

    액션바 일괄적용 - 액션바가 없는 액티비티는 적용 안함

--- app/src/main/java/com/jisu/apipractice_20200615/BaseActivity.kt
+++ app/src/main/java/com/jisu/apipractice_20200615/BaseActivity.kt
@@ -1,5 +1,7 @@
 package com.jisu.apipractice_20200615
 
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -7,4 +9,29 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    // BaseActivity를 상속받는 모든 액티비티는 자동으로 실행 처리
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 액션바가 있는 액티비티들만 커스텀 액션바 세팅 진행
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
+    }
+
+    // 액션바 관련 세팅 변경 함수
+    fun setCustomActionBar(){
+
+        // 액션바 커스텀 기능 활성화
+        supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        supportActionBar?.setCustomView(R.layout.custom_action_bar)
+
+        // 커스텀 액션바 영역 확장 => 뒷단 여백 제거
+        supportActionBar?.setBackgroundDrawable(null)
+
+        // 실제 여백 제거 => 커스텀 뷰를 감싸는 Toolbar 의 내부 여백값 0으로 설정
+        val parent = supportActionBar?.customView?.parent as androidx.appcompat.widget.Toolbar
+        parent.setContentInsetsAbsolute(0,0)
+    }
 }
--- app/src/main/java/com/jisu/apipractice_20200615/MainActivity.kt
+++ app/src/main/java/com/jisu/apipractice_20200615/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
-        setCustomActionBar()
     }
 
     override fun setupEvents() {
@@ -111,20 +110,5 @@ class MainActivity : BaseActivity() {
             }
 
         })
-    }
-
-    // 액션바 관련 세팅 변경 함수
-    fun setCustomActionBar(){
-
-        // 액션바 커스텀 기능 활성화
-        supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
-        supportActionBar?.setCustomView(R.layout.custom_action_bar)
-
-        // 커스텀 액션바 영역 확장 => 뒷단 여백 제거
-        supportActionBar?.setBackgroundDrawable(null)
-
-        // 실제 여백 제거 => 커스텀 뷰를 감싸는 Toolbar 의 내부 여백값 0으로 설정
-        val parent = supportActionBar?.customView?.parent as androidx.appcompat.widget.Toolbar
-        parent.setContentInsetsAbsolute(0,0)
     }
 }
